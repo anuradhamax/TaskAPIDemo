c3098fdb008c2f9e1f755e5ae2a0f2bbb0fe3333
anuradhamax <ranga.anuradha@gmail.com>
2021-10-25 11:25:17 +0530

    created a service

---
+++ TaskAPI/Controllers/ToDosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+using TaskAPI.Services;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private ToDoService _toDoService;
+        public ToDosController()
+        {
+            _toDoService = new ToDoService();
+        }
+        [HttpGet("{id?}")]
+        public IActionResult getTodos(int? id)
+        {
+            var myToDos = _toDoService.allToDos();
+
+            if (id is null) return Ok(myToDos);
+
+            myToDos = myToDos.Where(t => t.Id == id).ToList();
+
+            return Ok(myToDos);
+        } 
+
+        
+    }
+}
---
+++ TaskAPI/Services/ToDoService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+
+namespace TaskAPI.Services
+{
+    public class ToDoService
+    {
+        public List<ToDo> allToDos()
+        {
+            var ToDos = new List<ToDo>();
+
+            var todo1 = new ToDo
+            {
+                Id = 1,
+                Title = "get books for school",
+                Description = "get some text books for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = ToDoStatus.New
+            };
+
+
+
+            var todo2 = new ToDo
+            {
+                Id = 2,
+                Title = "get foods for school",
+                Description = "get some foods for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = ToDoStatus.New
+            };
+
+            var todo3 = new ToDo
+            {
+                Id = 3,
+                Title = "get drinks for school",
+                Description = "get some drink bottles for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = ToDoStatus.Completed
+            };
+
+            ToDos.Add(todo1);
+            ToDos.Add(todo2);
+            ToDos.Add(todo3);
+
+            return ToDos;
+        }
+    }
+}
